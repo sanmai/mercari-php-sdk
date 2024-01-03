c6dd1b597c8c9622672e7d6122223d83f8b3155d
Alexey Kopytko <alexey@kopytko.com>
2024-01-03 17:56:25 +0900

    WebhookSignature

---
+++ src/WebhookSignature.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Mercari PHP SDK
+ * Copyright 2024 Alexey Kopytko
+ *
+ * Mercari PHP SDK is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * Mercari PHP SDK is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Mercari PHP SDK.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Mercari;
+
+use Tumblr\Chorus\TimeKeeper;
+
+class WebhookSignature
+{
+    public const VALIDITY_WINDOW = 300;
+
+    private const VERSION = 'v0';
+
+    private string $signingSecret;
+    private string $requestBody;
+    private int $timestamp;
+    private string $signature;
+
+    public function __construct(string $signingSecret, ?string $requestBody = null, ?int $timestamp = null, ?string $signature = null)
+    {
+        $this->signingSecret = $signingSecret;
+        $this->requestBody = $requestBody ?? file_get_contents('php://input');
+
+        // X-Mercari-Request-Timestamp
+        $this->timestamp = $timestamp ?? intval($_SERVER['HTTP_X_MERCARI_REQUEST_TIMESTAMP'] ?? 0);
+
+        // X-Mercari-Signature
+        $this->signature = $signature ?? $_SERVER['HTTP_X_MERCARI_SIGNATURE'] ?? '';
+    }
+
+    public function isValid(?TimeKeeper $timekeeper = null, int $validityWindow = self::VALIDITY_WINDOW): bool
+    {
+        $timekeeper ??= new TimeKeeper();
+
+        if (abs($this->timestamp - $timekeeper->getCurrentUnixTime()) > $validityWindow) {
+            return false;
+        }
+
+        $hash = hash_hmac('sha256', join(':', [
+            self::VERSION,
+            $this->timestamp,
+            $this->requestBody,
+        ]), $this->signingSecret);
+
+        return hash_equals(join(':', [
+            self::VERSION,
+            $hash,
+        ]), $this->signature);
+    }
+}
---
+++ tests/WebhookSignatureTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Mercari PHP SDK
+ * Copyright 2024 Alexey Kopytko
+ *
+ * Mercari PHP SDK is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * Mercari PHP SDK is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Mercari PHP SDK.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Tests\Mercari;
+
+use Mercari\WebhookSignature;
+use Tumblr\Chorus\FakeTimeKeeper;
+
+/**
+ * @covers \Mercari\WebhookSignature
+ */
+class WebhookSignatureTest extends TestCase
+{
+    public function testInvalid()
+    {
+        $signature = new WebhookSignature('123', '{}', 123, '123');
+
+        $this->assertFalse($signature->isValid(new FakeTimeKeeper(123)));
+    }
+
+    public function testInvalidNow()
+    {
+        $signature = new WebhookSignature('456', '{}', 123, '567');
+
+        $this->assertFalse($signature->isValid());
+    }
+
+    private const TEST_TIME = 1531420618;
+
+    public static function provideTimestamps(): iterable
+    {
+        $time = self::TEST_TIME;
+
+        yield 'on time' => [$time, true];
+
+        yield 'on time + 1' => [$time + 1, true];
+
+        yield 'on time - 1' => [$time - 1, true];
+
+        yield 'minus window' => [$time - WebhookSignature::VALIDITY_WINDOW, true];
+        yield 'plus window' => [$time + WebhookSignature::VALIDITY_WINDOW, true];
+
+        yield 'under window' => [$time - WebhookSignature::VALIDITY_WINDOW - 1, false];
+        yield 'beyond window' => [$time + WebhookSignature::VALIDITY_WINDOW + 1, false];
+    }
+
+    /**
+     * @dataProvider provideTimestamps
+     */
+    public function testValid(int $time, bool $valid)
+    {
+        $timekeeper = new FakeTimeKeeper($time);
+
+        $signature = new WebhookSignature(
+            '8f742231b10e8888abcd99yyyzzz85a5',
+            '{"webhook_type":"test_webhook"}',
+            self::TEST_TIME,
+            'v0:249e47edc1980531306517e4435b54ef1ff224020029284bdf19c8eda99aa325'
+        );
+
+        $this->assertSame($valid, $signature->isValid($timekeeper));
+    }
+}
