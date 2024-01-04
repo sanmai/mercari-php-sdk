f2643339d46ed14e703ce9988fdc9e9d2112c590
Alexey Kopytko <alexey@kopytko.com>
2024-01-04 10:34:29 +0900

    CI Workflow (#1)

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Mercari PHP SDK
+ * Copyright 2024 Alexey Kopytko
+ *
+ * Mercari PHP SDK is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * Mercari PHP SDK is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Mercari PHP SDK. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+$header = trim("
+Mercari PHP SDK
+Copyright 2024 Alexey Kopytko
+
+Mercari PHP SDK is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 2 of the License, or (at your option) any later version.
+
+Mercari PHP SDK is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Mercari PHP SDK. If not, see <https://www.gnu.org/licenses/>.
+");
+
+use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
+
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'header_comment' => ['comment_type' => 'PHPDoc', 'header' => $header, 'separate' => 'bottom', 'location' => 'after_open'],
+        '@PER-CS' => true,
+        '@PER-CS:risky' => true,
+        '@PHP74Migration' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+        ->in(__DIR__)
+        ->append([__FILE__])
+    )
+;
+
+return $config;
