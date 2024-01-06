f7653ce2754dccc4b2666ff74ad11304a2e34c46
Alexey Kopytko <alexey@kopytko.com>
2024-01-06 23:37:16 +0900

    AuthClient (#14)

--- src/MercariAuthClient.php
+++ src/MercariAuthClient.php
@@ -20,16 +20,95 @@ declare(strict_types=1);
 namespace Mercari;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Utils;
+use GuzzleHttp\Exception\RequestException;
 use JMS\Serializer\SerializerInterface;
 use JSONSerializer\Serializer;
+use Tumblr\Chorus\TimeKeeper;
 
 /**
  * Authentication client for Mercari API.
  */
 class MercariAuthClient
 {
-    private const TOKEN = '/jp/v1/token';
+    public const TOKEN = '/jp/v1/token';
 
-    private const REDIRECT = '/jp/v1/authorize';
+    public const REDIRECT = '/jp/v1/authorize';
+
+    public static function createInstance(string $authHost, string $clientId, string $clientSecret, array $extraHeaders = []): self
+    {
+        $httpClient = new Client([
+            'debug' => defined('MERCARI_DEBUG_CURL'),
+            'base_uri' => sprintf('https://%s', $authHost),
+            'auth' => [$clientId, $clientSecret],
+            'connect_timeout' => 3,
+            'timeout' => 10,
+            'http_errors' => true,
+            'allow_redirects' => false,
+            'headers' => $extraHeaders,
+        ]);
+
+        return new MercariAuthClient(
+            $clientId,
+            $httpClient
+        );
+    }
+
+    private string $clientId;
+
+    private Client $client;
+
+    private SerializerInterface $serializer;
+
+    private TimeKeeper $timekeeper;
+
+    /**
+     * Create a new instance.
+     */
+    public function __construct(
+        string $clientId,
+        Client $client,
+        SerializerInterface $serializer = null,
+        TimeKeeper $timekeeper = null
+    ) {
+        $this->clientId = $clientId;
+        $this->client = $client;
+        $this->serializer = $serializer ?? Serializer::withJSONOptions();
+        $this->timekeeper = $timekeeper ?? new TimeKeeper();
+    }
+
+    /**
+     * @throws RequestException
+     */
+    public function getToken(TokenRequest $request = null): TokenResponse
+    {
+        $request ??= TokenRequest::clientCredentials();
+
+        $response = $this->client->post(
+            self::TOKEN,
+            ['form_params' => $request->getRequestParams()]
+        );
+
+        $content = $response->getBody()->getContents();
+
+        $token = $this->serializer->deserialize($content, TokenResponse::class, 'json');
+        $token->ts = $this->timekeeper->getCurrentUnixTime();
+
+        return $token;
+    }
+
+    /**
+     * @throws RequestException
+     */
+    public function getAuthUrl(TokenRequest $request): ?string
+    {
+        $request->client_id = $this->clientId;
+
+        return $this->client->get(
+            self::REDIRECT,
+            [
+                'query' => $request->getRequestParams(),
+                'auth' => null,
+            ]
+        )->getHeader('Location')[0];
+    }
 }
