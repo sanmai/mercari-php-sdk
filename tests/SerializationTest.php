65b01c4b0d49c19168ee1682d6d78e5fcbe19e9f
Alexey Kopytko <alexey@kopytko.com>
2024-01-07 12:54:39 +0900

    Improve SerializationTest (#16)

--- tests/SerializationTest.php
+++ tests/SerializationTest.php
@@ -34,6 +34,7 @@ use Mercari\TodoListResponse;
 use PHPUnit\Framework\AssertionFailedError;
 use IteratorAggregate;
 use Countable;
+use Iterator;
 
 class SerializationTest extends TestCase
 {
@@ -108,10 +109,12 @@ class SerializationTest extends TestCase
         $response = $this->deserializeFile($file, $className);
 
         if ($response instanceof IteratorAggregate) {
+            $this->assertInstanceOf(Iterator::class, $response->getIterator());
             $this->assertGreaterThanOrEqual(0, iterator_count($response));
         }
 
         if ($response instanceof Countable) {
+            $this->assertIsInt($response->count());
             $this->assertGreaterThanOrEqual(0, count($response));
         }
 
