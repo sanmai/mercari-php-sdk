a88421994b39321b2dbfff399cdcff51c97b713a
Alexey Kopytko <alexey@kopytko.com>
2026-07-03 20:37:34 +0900

    Apply suggestion from @sanmai

--- src/AbstractMercariClient.php
+++ src/AbstractMercariClient.php
@@ -102,7 +102,7 @@ abstract class AbstractMercariClient
         string $type,
         string $uri,
         array $query = [],
-        array $headers = []
+        array $headers = [],
     ) {
         $response = $this->client->get($uri, [
             'query' => $query,
